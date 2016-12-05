f2cb2c7b86f4154dfce45a9a986644c28ed1032c
刘源 <405653510@qq.com>
2016-12-05 16:04:08 +0800

    提交

--- DotNetty_NLog/Program.cs
+++ DotNetty_NLog/Program.cs
@@ -6,7 +6,7 @@ using DotNetty_NLog.handler;
 using Microsoft.Extensions.Logging;
 using NLog;
 using System;
-using System.Collections.Generic;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,41 +17,42 @@ namespace DotNetty_NLog
     {
         private static Logger LOGGER = LogManager.GetCurrentClassLogger();
 
-        static ISettingsProvider settingsProvider = new AppConfigSettingsProvider();
+
 
 
         static async Task RunServerAsync()
         {
-            String getewayPort = settingsProvider.GetSetting("Gateway.Port");
-            var bossGroup = new MultithreadEventLoopGroup(1);
-            var workerGroup = new MultithreadEventLoopGroup(Environment.ProcessorCount);
-            try
-            {
-                var bootstrap = new ServerBootstrap();
-                bootstrap
-                    .Group(bossGroup, workerGroup)
-                    .Channel<TcpServerSocketChannel>()
-                    .Option(ChannelOption.SoBacklog, 512)
-                    .ChildOption(ChannelOption.TcpNodelay, true)
-                    .Option(ChannelOption.SoReuseaddr, true)
-                    .ChildHandler(new ActionChannelInitializer<ISocketChannel>(channel =>
-                    {
-                        IChannelPipeline pipeline = channel.Pipeline;
-                        pipeline.AddLast(new CustomProtocolDecoder());
-                        pipeline.AddLast(new CustomProtocolEncoder());
-                        pipeline.AddLast(new FrontDeviceHandler());
-                    }));
+            //String getewayPort = ConfigurationManager.AppSettings["Gateway.Port"];
+            //String getewayPort = "5061";
+            //var bossGroup = new MultithreadEventLoopGroup(1);
+            //var workerGroup = new MultithreadEventLoopGroup(Environment.ProcessorCount);
+            //try
+            //{
+            //    var bootstrap = new ServerBootstrap();
+            //    bootstrap
+            //        .Group(bossGroup, workerGroup)
+            //        .Channel<TcpServerSocketChannel>()
+            //        .Option(ChannelOption.SoBacklog, 512)
+            //        .ChildOption(ChannelOption.TcpNodelay, true)
+            //        .Option(ChannelOption.SoReuseaddr, true)
+            //        .ChildHandler(new ActionChannelInitializer<ISocketChannel>(channel =>
+            //        {
+            //            IChannelPipeline pipeline = channel.Pipeline;
+            //            pipeline.AddLast(new CustomProtocolDecoder());
+            //            pipeline.AddLast(new CustomProtocolEncoder());
+            //            pipeline.AddLast(new FrontDeviceHandler());
+            //        }));
 
-                IChannel boundChannel = await bootstrap.BindAsync(int.Parse(getewayPort));
-                Console.ReadLine();
-                await boundChannel.CloseAsync();
-            }
-            finally
-            {
-                await Task.WhenAll(
-                    workerGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)),
-                    bossGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)));
-            }
+            //    IChannel boundChannel = await bootstrap.BindAsync(int.Parse(getewayPort));
+            //    Console.ReadLine();
+            //    await boundChannel.CloseAsync();
+            //}
+            //finally
+            //{
+            //    await Task.WhenAll(
+            //        workerGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)),
+            //        bossGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)));
+            //}
         }
 
         static void Main(string[] args)
